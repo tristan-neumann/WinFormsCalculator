852b968043aeb4520c278a34cfb30ba9fc2b2bf8
Tristan Neumann <tristan.neumann@brickmakers.de>
2019-04-10 13:23:21 +0200

    parse() deleted, Interface added

--- WindowsFormsCalculator/WindowsFormsCalculator/WindowsFormsCalculatorModel.cs
+++ WindowsFormsCalculator/WindowsFormsCalculator/WindowsFormsCalculatorModel.cs
@@ -7,50 +7,16 @@ using System.Threading.Tasks;
 
 namespace WindowsFormsCalculator
 {
-    public class WindowsFormsCalculatorModel
+    public interface IWindowsFormsCalculatorModel
     {
+        double sum(double a, double b);
+        double subtract(double a, double b);
+        double multiply(double a, double b);
+        double divide(double a, double b);
+    }
 
-        public double parse(string input)
-        {
-            if (input.Contains("X"))
-            {
-                var splitResult = input.Split('X');
-
-                return Double.Parse(splitResult[0]) * Double.Parse(splitResult[1]);
-            }
-            else if (input.Contains("/"))
-            {
-                var splitResult = input.Split('/');
-
-                return Double.Parse(splitResult[0]) / Double.Parse(splitResult[1]);
-            }
-            else if (input.Contains("+"))
-            {
-                var splitResult = input.Split('+');
-
-                return Double.Parse(splitResult[0]) + Double.Parse(splitResult[1]);
-            }
-            else if (input.Contains("-"))
-            {
-                if (input[0] == '-')
-                {
-                    input = input.Substring(1);
-                }
-                var splitResult = input.Split('-');
-
-                if (input.Contains("-")) {
-                    return -Double.Parse(splitResult[0]) - Double.Parse(splitResult[1]);
-                }
-                else
-                {
-                    return -Double.Parse(input);
-                }
-            }
-            else
-            {
-                return Double.Parse(input);
-            }
-        }
+    public class WindowsFormsCalculatorModel : IWindowsFormsCalculatorModel
+    {
 
         public double sum(double a, double b)
         {
