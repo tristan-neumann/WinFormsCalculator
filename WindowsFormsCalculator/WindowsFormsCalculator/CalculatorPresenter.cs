374f6e4d1c4e4516fe92d846190cc0a6b24be712
Tristan Neumann <tristan.neumann@brickmakers.de>
2019-04-15 13:43:17 +0200

    implemented new Presenter/TDD

---
+++ WindowsFormsCalculator/WindowsFormsCalculator/CalculatorPresenter.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Globalization;
+
+namespace WindowsFormsCalculator
+{
+    public class CalculatorPresenter : ICalculatorPresenter
+    {
+        private enum CalcOperator
+        {
+            Plus, Minus, DividedBy, MultipliedBy, Empty
+        }
+
+        private readonly ICalculatorView _calculatorView;
+        private string _numberString;
+        private double _number;
+        private CalcOperator _calcOperator;
+
+        public CalculatorPresenter(ICalculatorView calculatorView)
+        {
+            _calculatorView = calculatorView;
+        }
+
+
+        public void OnButton0Clicked()
+        {
+            _numberString += "0";
+            _calculatorView.AppendToOutput("0");
+        }
+
+        public void OnButton1Clicked()
+        {
+            _numberString += "1";
+            _calculatorView.AppendToOutput("1");
+        }
+
+        public void OnButton2Clicked()
+        {
+            _numberString += "2";
+            _calculatorView.AppendToOutput("2");
+        }
+
+        public void OnButton3Clicked()
+        {
+            _numberString += "3";
+            _calculatorView.AppendToOutput("3");
+        }
+
+        public void OnButton4Clicked()
+        {
+            _numberString += "4";
+            _calculatorView.AppendToOutput("4");
+        }
+
+        public void OnButton5Clicked()
+        {
+            _numberString += "5";
+            _calculatorView.AppendToOutput("5");
+        }
+
+        public void OnButton6Clicked()
+        {
+            _numberString += "6";
+            _calculatorView.AppendToOutput("6");
+        }
+
+        public void OnButton7Clicked()
+        {
+            _numberString += "7";
+            _calculatorView.AppendToOutput("7");
+        }
+
+        public void OnButton8Clicked()
+        {
+            _numberString += "8";
+            _calculatorView.AppendToOutput("8");
+        }
+
+        public void OnButton9Clicked()
+        {
+            _numberString += "9";
+            _calculatorView.AppendToOutput("9");
+        }
+
+        public void OnButtonPlusClicked()
+        {
+            CalculateResult();
+            _calcOperator = CalcOperator.Plus;
+            _calculatorView.AppendToOutput("+");
+        }
+
+        public void OnButtonMinusClicked()
+        {
+            CalculateResult();
+            _calcOperator = CalcOperator.Minus;
+            _calculatorView.AppendToOutput("-");
+        }
+
+        public void OnButtonMultipliedByClicked()
+        {
+            CalculateResult();
+            _calcOperator = CalcOperator.MultipliedBy;
+            _calculatorView.AppendToOutput("*");
+        }
+
+        public void OnButtonDividedByClicked()
+        {
+            CalculateResult();
+            _calcOperator = CalcOperator.DividedBy;
+            _calculatorView.AppendToOutput("/");
+        }
+
+        public void OnButtonEqualClicked()
+        {
+            CalculateResult();
+        }
+
+        private void CalculateResult()
+        {
+            var number = double.Parse(_numberString);
+            _numberString = string.Empty;
+
+            switch (_calcOperator)
+            {
+                case CalcOperator.Plus:
+                    _number += number;
+                    break;
+                case CalcOperator.Minus:
+                    _number -= number;
+                    break;
+                case CalcOperator.DividedBy:
+                    _number /= number;
+                    break;
+                case CalcOperator.MultipliedBy:
+                    _number *= number;
+                    break;
+                case CalcOperator.Empty:
+                    _number = number;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
+            _calculatorView.SetOutput(_number.ToString(CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ WindowsFormsCalculator/WindowsFormsCalculatorTests/CalculatorPresenterTests.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsFormsCalculator;
+using Moq;
+using NUnit.Framework;
+
+namespace WindowsFormsCalculatorTests
+{
+    [TestFixture]
+    public class CalculatorPresenterTests
+    {
+        private Mock<ICalculatorView> _calculatorViewMock;
+        private CalculatorPresenter _cut;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _calculatorViewMock = new Mock<ICalculatorView>();
+            _cut = new CalculatorPresenter(_calculatorViewMock.Object);
+        }
+
+        [TestFixture]
+        public class BasicFunctionalityTests : CalculatorPresenterTests
+        {
+            [Test]
+            public void OnButtonPlusClickedTest()
+            {
+                _cut.OnButton3Clicked();
+                _cut.OnButton0Clicked();
+
+                _cut.OnButtonPlusClicked();
+
+                _cut.OnButton4Clicked();
+
+                _cut.OnButtonEqualClicked();
+
+                _calculatorViewMock.Verify(m => m.SetOutput("34"));
+            }
+
+            [Test]
+            public void OnButtonMinusClickedTest()
+            {
+                _cut.OnButton3Clicked();
+                _cut.OnButton0Clicked();
+
+                _cut.OnButtonMinusClicked();
+
+                _cut.OnButton4Clicked();
+
+                _cut.OnButtonEqualClicked();
+
+                _calculatorViewMock.Verify(m => m.SetOutput("26"));
+            }
+
+            [Test]
+            public void OnButtonMultipliedByClickedTest()
+            {
+                _cut.OnButton3Clicked();
+                _cut.OnButton0Clicked();
+
+                _cut.OnButtonMultipliedByClicked();
+
+                _cut.OnButton4Clicked();
+
+                _cut.OnButtonEqualClicked();
+
+                _calculatorViewMock.Verify(m => m.SetOutput("120"));
+            }
+
+            [Test]
+            public void OnButtonDividedByClickedTest()
+            {
+                _cut.OnButton4Clicked();
+                _cut.OnButton0Clicked();
+
+                _cut.OnButtonDividedByClicked();
+
+                _cut.OnButton4Clicked();
+
+                _cut.OnButtonEqualClicked();
+
+                _calculatorViewMock.Verify(m => m.SetOutput("10"));
+            }
+
+            [Test]
+            public void MultipleInputTest1()
+            {
+                _cut.OnButton3Clicked();
+                _cut.OnButton0Clicked();
+
+                _cut.OnButtonMultipliedByClicked();
+
+                _cut.OnButton4Clicked();
+
+                _cut.OnButtonPlusClicked();
+
+                _cut.OnButton5Clicked();
+
+                _cut.OnButtonEqualClicked();
+                _calculatorViewMock.Verify(m => m.SetOutput("125"));
+            }
+
+            [Test]
+            public void MultipleInputTest2()
+            {
+                _cut.OnButton3Clicked();
+                _cut.OnButton0Clicked();
+
+                _cut.OnButtonMultipliedByClicked();
+
+                _cut.OnButton4Clicked();
+
+                _cut.OnButtonMinusClicked();
+
+                _cut.OnButton5Clicked();
+
+                _cut.OnButtonEqualClicked();
+                _calculatorViewMock.Verify(m => m.SetOutput("115"));
+            }
+
+            [Test]
+            public void MultipleInputTest3()
+            {
+                _cut.OnButton3Clicked();
+                _cut.OnButton0Clicked();
+
+                _cut.OnButtonMultipliedByClicked();
+
+                _cut.OnButton4Clicked();
+
+                _cut.OnButtonMultipliedByClicked();
+
+                _cut.OnButton2Clicked();
+
+                _cut.OnButtonEqualClicked();
+                _calculatorViewMock.Verify(m => m.SetOutput("240"));
+            }
+
+            [Test]
+            public void MultipleInputTest4()
+            {
+                _cut.OnButton3Clicked();
+                _cut.OnButton0Clicked();
+
+                _cut.OnButtonMultipliedByClicked();
+
+                _cut.OnButton4Clicked();
+
+                _cut.OnButtonDividedByClicked();
+
+                _cut.OnButton6Clicked();
+
+                _cut.OnButtonEqualClicked();
+                _calculatorViewMock.Verify(m => m.SetOutput("20"));
+            }
+        }
+
+        [TestFixture]
+        public class BasicViewTests : CalculatorPresenterTests
+        {
+            [Test]
+            public void AppendOutputTest0()
+            {
+                _cut.OnButton0Clicked();
+                _calculatorViewMock.Verify(m => m.AppendToOutput("0"));
+            }
+
+            [Test]
+            public void AppendOutputTest1()
+            {
+                _cut.OnButton1Clicked();
+                _calculatorViewMock.Verify(m => m.AppendToOutput("1"));
+            }
+
+            [Test]
+            public void AppendOutputTest2()
+            {
+                _cut.OnButton2Clicked();
+                _calculatorViewMock.Verify(m => m.AppendToOutput("2"));
+            }
+
+            [Test]
+            public void AppendOutputTest3()
+            {
+                _cut.OnButton3Clicked();
+                _calculatorViewMock.Verify(m => m.AppendToOutput("3"));
+            }
+
+            [Test]
+            public void AppendOutputTest4()
+            {
+                _cut.OnButton4Clicked();
+                _calculatorViewMock.Verify(m => m.AppendToOutput("4"));
+            }
+
+            [Test]
+            public void AppendOutputTest5()
+            {
+                _cut.OnButton5Clicked();
+                _calculatorViewMock.Verify(m => m.AppendToOutput("5"));
+            }
+
+            [Test]
+            public void AppendOutputTest6()
+            {
+                _cut.OnButton6Clicked();
+                _calculatorViewMock.Verify(m => m.AppendToOutput("6"));
+            }
+
+            [Test]
+            public void AppendOutputTest7()
+            {
+                _cut.OnButton7Clicked();
+                _calculatorViewMock.Verify(m => m.AppendToOutput("7"));
+            }
+
+            [Test]
+            public void AppendOutputTest8()
+            {
+                _cut.OnButton8Clicked();
+                _calculatorViewMock.Verify(m => m.AppendToOutput("8"));
+            }
+
+            [Test]
+            public void AppendOutputTest9()
+            {
+                _cut.OnButton9Clicked();
+                _calculatorViewMock.Verify(m => m.AppendToOutput("9"));
+            }
+
+            [Test]
+            public void AppendOutputPlusTest()
+            {
+                _cut.OnButton0Clicked();
+                _cut.OnButtonPlusClicked();
+                _calculatorViewMock.Verify(m => m.AppendToOutput("+"));
+            }
+
+            [Test]
+            public void AppendOutputMinusTest()
+            {
+                _cut.OnButton0Clicked();
+                _cut.OnButtonMinusClicked();
+                _calculatorViewMock.Verify(m => m.AppendToOutput("-"));
+            }
+
+            [Test]
+            public void AppendOutputMultipliedByTest()
+            {
+                _cut.OnButton0Clicked();
+                _cut.OnButtonMultipliedByClicked();
+                _calculatorViewMock.Verify(m => m.AppendToOutput("*"));
+            }
+
+            [Test]
+            public void AppendOutputDividedByTest()
+            {
+                _cut.OnButton0Clicked();
+                _cut.OnButtonDividedByClicked();
+                _calculatorViewMock.Verify(m => m.AppendToOutput("/"));
+            }
+        }
+    }
+}
