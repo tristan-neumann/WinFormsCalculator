9fa46cda191a41af8fc07dfde763fed7e909b316
Tristan Neumann <tristan.neumann@brickmakers.de>
2019-04-15 13:42:05 +0200

    Interfaces added

---
+++ WindowsFormsCalculator/WindowsFormsCalculator/ICalculatorPresenter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsCalculator
+{
+    public interface ICalculatorPresenter
+    {
+        void OnButton0Clicked();
+        void OnButton1Clicked();
+        void OnButton2Clicked();
+        void OnButton3Clicked();
+        void OnButton4Clicked();
+        void OnButton5Clicked();
+        void OnButton6Clicked();
+        void OnButton7Clicked();
+        void OnButton8Clicked();
+        void OnButton9Clicked();
+
+        void OnButtonPlusClicked();
+        void OnButtonMinusClicked();
+        void OnButtonMultipliedByClicked();
+        void OnButtonDividedByClicked();
+
+        void OnButtonEqualClicked();
+    }
+}
---
+++ WindowsFormsCalculator/WindowsFormsCalculator/ICalculatorView.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsCalculator
+{
+    public interface ICalculatorView
+    {
+        void SetOutput(string output);
+        void AppendToOutput(string output);
+    }
+}
